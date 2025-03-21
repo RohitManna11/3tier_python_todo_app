cb216c9e145c7af44d71a6aa8ca7fcaa82cb5d33
RohitManna11 <rkmanna11@gmail.com>
2025-03-21 14:08:45 +0000

    backend cors issue solved

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app)  
+CORS(app, resources={r"/*": {"origins": "*"}}) 
 
 # Database Configuration
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
